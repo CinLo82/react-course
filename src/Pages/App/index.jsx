ebd93df931c7f980c5140553ea9b83067586b734
Cin💜 Lo <cinlos3382@gmail.com>
2023-07-02 14:54:39 -0300

    contexto global de la app

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -6,6 +6,7 @@ import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
 import Navbar from '../../components/Navbar'
+import { ShoppingCartProvider } from '../../Context'
 import './App.css'
 
 const AppRouters = () => {
@@ -23,10 +24,12 @@ const AppRouters = () => {
 const App = () => {
  
 	return (
-		<BrowserRouter>
-			<AppRouters />
-			<Navbar />
-		</BrowserRouter>
+		<ShoppingCartProvider>
+			<BrowserRouter>
+				<AppRouters />
+				<Navbar />
+			</BrowserRouter>
+		</ShoppingCartProvider>
 	)
 }
 
