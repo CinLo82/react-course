e0a8398b9b55e375bdcffc4e71919fb50b7ebe14
Cin💜 Lo <cinlos3382@gmail.com>
2023-07-02 21:15:58 -0300

    abrimos y cerramos product detail, dandole funcionalidad a los icons

--- src/components/ProductDetail/index.jsx
+++ src/components/ProductDetail/index.jsx
@@ -1,14 +1,19 @@
-
+import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
-
+import { ShoppingCartContext } from "../../Context"
 import './styles.css'
 
 const ProductDetail = () => {
+    const {isProductDetailOpen, closeProductDetail} = useContext(ShoppingCartContext)
+
     return (
-        <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white '>
+        <aside className={`${isProductDetailOpen ? 'flex' : 'hidden'} product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}>
             <div className='flex justify-between items-center p-6'>
                 <h2 className='font-medium text-xl'>Detail</h2>
-                <XMarkIcon className="h-6 w-6 text-red-500" />
+                <XMarkIcon 
+                    className="h-6 w-6 text-red-500" 
+                    onClick={() => closeProductDetail()} 
+                />
             
             </div>
         </aside>
